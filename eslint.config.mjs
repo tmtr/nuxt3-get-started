483154059d476c4f51762613dca8fd7c776bf9cc
tmtr <tera@tmtr.jp>
2024-05-13 17:02:22 +0900

    chore: eslint.config.mjs を追加
    * ルールなどは記事コンテンツのものから流用

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  {
+    files: [
+      '**/*.?(c,m)js',
+      '**/*.ts?(x)',
+      '**/*.vue'
+    ],
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+      '@typescript-eslint/member-delimiter-style': ['error', {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'default',
+          format: ['camelCase', 'PascalCase', 'UPPER_CASE']
+        },
+        {
+          selector: 'variable',
+          format: ['camelCase', 'UPPER_CASE', 'PascalCase']
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow'
+        },
+        {
+          selector: 'memberLike',
+          modifiers: ['private'],
+          format: ['camelCase'],
+          leadingUnderscore: 'require'
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase']
+        }
+      ],
+      '@typescript-eslint/no-empty-interface': 'off'
+    }
+  },
+  {
+    files: [
+      '**/*.vue'
+    ],
+    rules: {
+      'vue/require-v-for-key': 'off'
+    }
+  }
+)
