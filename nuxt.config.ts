2ab61fdd01a2de25adf0c7af4eaa3abf1b8fec13
tmtr <tera@tmtr.jp>
2024-05-15 16:58:10 +0900

    chore: scss のグローバル変数やスタイルを *.vue 内で扱うテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,5 +16,16 @@ export default defineNuxtConfig({
     config: {
       stylistic: true
     }
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // NOTE: グローバルに定義した scss 変数や mixin を *.vue 内で使用したい場合には、ここに scss ファイルのパスを追加する
+          additionalData: '@import "~/assets/stylesheets/_foundations.scss";'
+        }
+      }
+    }
   }
 })
