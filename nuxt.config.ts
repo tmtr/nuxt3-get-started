9adac5704b890d6d32292084c33a023946f4bf25
tmtr <tera@tmtr.jp>
2024-05-15 17:09:55 +0900

    chore: CSS module 使用時のクラス名をカスタマイズ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {
@@ -20,6 +22,24 @@ export default defineNuxtConfig({
 
   vite: {
     css: {
+      modules: {
+        // NOTE: CSS module のクラス名をカスタマイズして、"[コンポーネント名]-[ローカルクラス名]--[ハッシュ]" に設定する
+        //       https://stackoverflow.com/a/77075108
+        generateScopedName: (name, filename, css) => {
+          const componentName = filename
+            .replace(/\.vue.+$/, '')
+            .split('/')
+            .pop()
+
+          const hash = crypto
+            .createHash('md5')
+            .update(css)
+            .digest('base64')
+            .substring(0, 5)
+
+          return `${componentName}-${name}--${hash}`
+        }
+      },
       preprocessorOptions: {
         scss: {
           // NOTE: グローバルに定義した scss 変数や mixin を *.vue 内で使用したい場合には、ここに scss ファイルのパスを追加する
